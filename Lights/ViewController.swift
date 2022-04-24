c79cbede72ef1d9ba312a11f1d4fc7bf6b9a18e9
Yana <yanka.pol@gmail.com>
2022-04-24 14:18:00 +0700

    setup main.storyboard 2

--- Lights/ViewController.swift
+++ Lights/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    
+    @IBOutlet var startButton: UIButton!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 10
+        
+        redLight.alpha = 1.0
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        
+        redLight.layer.cornerRadius = redLight.frame.height/2
+        yellowLight.layer.cornerRadius = yellowLight.frame.height/2
+        greenLight.layer.cornerRadius = greenLight.frame.height/2
     }
 
+    
+    @IBAction func switchLights() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        if redLight.alpha == 1.0 {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1.0
+            return
+        }
+        
+        if yellowLight.alpha == 1.0 {
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1.0
+            return
+        }
 
+        if greenLight.alpha == 1.0 {
+            greenLight.alpha = 0.3
+            redLight.alpha = 1.0
+            return
+        }
+        
+        else {
+            redLight.alpha = 1.0
+            return 
+        }
+    }
 }
-
+       
+ 
